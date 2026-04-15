1c95ea742b8d6f1d6453b5a63c1b618ca2c28d97
Patricio Brevis <brevis.acuna@gmail.com>
2026-04-15 09:41:48 -0400

    Update web-app-analisis-gotas.py

--- web-app-analisis-gotas.py
+++ web-app-analisis-gotas.py
@@ -7,7 +7,7 @@ from statistics import median
 import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
-import pdfkit
+from weasyprint import HTML
 
 # Page configuration
 ic = Image.open('figs/bar-chart-icon.png')
@@ -179,14 +179,17 @@ def main():
     </html>
     """
 
-    config = pdfkit.configuration()
-    pdf_bytes = pdfkit.from_string(contenido_html, False,
-                                   configuration=config, options=formato)
+    # config = pdfkit.configuration()
+    # pdf_bytes = pdfkit.from_string(contenido_html, False,
+    #                                configuration=config, options=formato)
 
+    pdf_file = HTML(string=contenido_html).write_pdf()
+
+  
     st.sidebar.text("")
     st.sidebar.text("DESCARGA TUS RESULTADOS")
-    st.sidebar.download_button(label='Bajar PDF', data=pdf_bytes,
-                       file_name="resultados.pdf", mime='application/octet-stream')
+    st.sidebar.download_button(label='Bajar PDF', data=pdf_file,
+                       file_name="resultados.pdf", mime='application/pdf')
 
     st.sidebar.text("")
     st.sidebar.text("")
